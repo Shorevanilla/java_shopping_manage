0fb320da8fde40df6ec8bd755ae7b82068ea39d7
Shorevanilla erier <2455745979@qq.com>
2023-07-11 06:04:24 +0000

    modified:   src/main/java/org/example/Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("欢迎使用购物管理系统，您可以输入 help 查看指令");
 
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
@@ -23,12 +23,18 @@ public class Main {
             if (userInput.equals("help")) {
                 Main.help();
             }
+            if (userInput.equals("ulogin")) {
+                Main.uEnter();
+            if (userInput.equals("mlogin")) {
+                    Main.mEnter();
+                }
+
         }
 
         scanner.close();
         System.out.println("Done.");
     }
-
+    }
     private static void help() {
         System.out.print("欢迎进入帮助子菜单");
 
@@ -46,5 +52,45 @@ public class Main {
 
             System.out.println("其实吧，这个也就是做个样子给你看看，让你知道怎么做二级界面罢了");
         }
+        scanner.close();
     }
+    private static void uEnter() {
+        System.out.print("********************登入********************");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        while(true) {
+            System.out.println("请输入你的指令,q 退出");
+            System.out.print("你当前在 uEnter 的二级子目录下 >");
+            userInput = scanner.nextLine();
+
+            if (userInput.equals("q")) {
+                break;
+            }
+
+            System.out.println("其实吧，这个也就是做个样子给你看看，让你知道怎么做二级界面罢了");
+        }
+        scanner.close();
+    }
+    private static void mEnter() {
+        System.out.print("********************登入********************");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        while(true) {
+            System.out.println("请输入你的指令,q 退出");
+            System.out.print("你当前在 mEnter 的二级子目录下 >");
+            userInput = scanner.nextLine();
+
+            if (userInput.equals("q")) {
+                break;
+            }
+
+            System.out.println("其实吧，这个也就是做个样子给你看看，让你知道怎么做二级界面罢了");
+        }
+        scanner.close();
+    }
+
 }
